57bee17eb1f023c7eb4d077d1117f2c6e1e5539d
shengfang198 <lharc3395@gmail.com>
2025-12-14 21:46:57 +0800

    Delete webportfolio/myPortfolio directory

--- webportfolio/myPortfolio/jcrtv.dev/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import { Config } from "tailwindcss";
-
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
--- webportfolio/myPortfolio/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/jcrtv.dev/', // Set base path for GitHub Pages deployment
-})
